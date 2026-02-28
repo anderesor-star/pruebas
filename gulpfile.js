cd657cbdf7e94521e1504357435fe073575bb2ef
anderesor-star <anderesor@gmail.com>
2026-02-28 21:43:16 +0100

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,10 @@ gulp.task('serve', function() {
   browserSync.init({
     server: {
       baseDir: "./"
+      //entre las comillas el .html que quiera que se actualice. Cada vez que quiera cambiar el doc tengo que cambiar este
     }
   });
 
   gulp.watch("*.html").on("change", reload);
 });
+//meter en el powershell "gulp serve" para que empiece. Ctr+C para pararlo
